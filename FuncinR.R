e56ec74e526105ecf95c60bf976451ad019af306
Aakash-garg54 <aakashgarg5518.com>
2021-01-13 23:21:04 +0530

    Adding up a new file and modify Func

---
+++ FuncinR.R
@@ -0,0 +1,5 @@
+# Function : In this, We have to load the function by clicking to the source and load the same. 
+#         After loading, we can use the function
+
+print(calc(10,5)) # Here, I have call a function named calc
+print(calc(10,2))

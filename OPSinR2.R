d3438a300460ef5268a92175e70d8c4c4a704125
Aakash-garg54 <aakashgarg5518.com>
2021-01-13 14:23:49 +0530

    Adding and modify a file

--- OPSinR2.R
+++ OPSinR2.R
@@ -12,3 +12,17 @@ print(length(mt)) # Returns elements of matrix
 print(mt[,1:2])
 print(mt[1:2,])
 print(mt[c("a","c"),c("d","f")])
+print(mt[1,])
+print(mt[,2])
+print(mt[,-2])
+print(mt[-2,])
+
+print("<-------Exercise------>")
+A = matrix(c(c(1,7,3),c(4,4,6),c(4,7,12)),nrow = 3, ncol = 3, byrow = T)
+print(A)
+A[3,3] = 13
+print(A)
+print(A[2,])
+print(A[,3])
+print(A[c(1,3),c(1,3)]) # This will skip the 2nd row and 2nd col
+print(A[c(1,2),c(1,3)])
---
+++ OPSinR3.R
@@ -0,0 +1,10 @@
+mt = matrix(c(c(1,2,3),c(4,5,6),c(7,8,9)),nrow = 3, ncol = 3, byrow = T)
+mt_R = matrix(c(10,11,12),1,3,byrow = T)
+print(mt_R)
+mtR = rbind(mt,mt_R)
+print(mtR)
+mt_C = matrix(c(10,11,12),3,1,byrow = T)
+print(mt_C)
+mtC = cbind(mt,mt_C)
+print(mtC)
+
